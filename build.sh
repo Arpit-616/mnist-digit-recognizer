f0650207d466c691ac0045a2e5997882ac875b2c
Arpit Srivastava <sriarpit05@gmail.com>
2025-10-20 03:11:40 +0530

    Simplify build script by removing unnecessary steps

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "Upgrading pip, setuptools, and wheel..."
-pip install --upgrade pip setuptools wheel
-
-echo "Installing dependencies..."
-pip install --no-cache-dir -r requirements.txt
-
-echo "Build completed successfully!"
+pip install --upgrade pip
+pip install -r requirements.txt
